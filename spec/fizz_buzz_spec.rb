0902fba2225ee10666daa9d44f9508a3f6f8bbe5
William Schneiderman <william.m.schneiderman@gmail.com>
2018-10-15 09:50:22 +0200

    added code to make it pass test

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -2,4 +2,20 @@ require './lib/fizz_buzz'
 
 describe 'fizz_buzz' do
 
+    it 'returns 1 if number is 1' do
+        expect(fizz_buzz(1)).to eq 1 
+    end
+
+    it "returns 'fizz' if number is divisible by 3" do
+        expect(fizz_buzz(3)).to eq 'fizz'
+      end
+
+      def fizz_buzz(number)
+        if number % 3 == 0
+          'fizz'
+        else
+          number
+        end 
+      end
+
 end
