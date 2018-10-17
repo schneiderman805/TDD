a48e8b69f0cad7a4d6a0071f7fa8756f6ce2c029
William Schneiderman <william.m.schneiderman@gmail.com>
2018-10-17 10:22:45 +0200

    finished refactoring

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,11 +1,15 @@
 def fizz_buzz(number)
-    if number % 15 == 0
+    if has_zero_remainder?(number, 15)
         'fizz buzz'
-    elsif number % 3 == 0
+    elsif has_zero_remainder?(number, 3)
         'fizz'
-    elsif number % 5 == 0
+    elsif has_zero_remainder?(number, 5)
         'buzz'
     else
         number
     end 
+end
+
+    def has_zero_remainder?(number, divider)
+    number % divider == 0
 end
